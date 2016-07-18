8a263421f154e286f7e6b5d2ea6e5062f5d9324a
asLody <imlody@foxmail.com>
2016-07-18 19:19:07 +0800

    Fix: dynamic premission may produce crash. Fix: a nullptr. Feature: add new Hook on AM.

--- VirtualApp/lib/src/main/java/com/lody/virtual/client/hook/patchs/pm/PackageManagerPatch.java
+++ VirtualApp/lib/src/main/java/com/lody/virtual/client/hook/patchs/pm/PackageManagerPatch.java
@@ -46,7 +46,8 @@ import java.lang.reflect.Field;
 		Hook_ClearPackagePreferredActivities.class, Hook_GetPreferredActivities.class,
 		Hook_ClearPackagePersistentPreferredActivities.class,
 
-		Hook_DeletePackage.class, Hook_GetPackageInstaller.class
+		Hook_DeletePackage.class, Hook_GetPackageInstaller.class, Hook_AddOnPermissionsChangeListener.class,
+		Hook_AddOnPermissionsChangeListener.class, Hook_RemoveOnPermissionsChangeListener.class,
 
 })
 public final class PackageManagerPatch extends PatchObject<IPackageManager, HookObject<IPackageManager>> {
