20fa7da6ab7f0d5dd5dcdea55b10b511509070b1
qiang.sheng <qiang.sheng@godinsec.com>
2016-07-22 16:17:01 +0800

    fix : 当android版本在5.0以上不能隐式启动的问题

--- VirtualApp/lib/src/main/java/com/lody/virtual/client/hook/patchs/am/Hook_BindService.java
+++ VirtualApp/lib/src/main/java/com/lody/virtual/client/hook/patchs/am/Hook_BindService.java
@@ -2,8 +2,10 @@ package com.lody.virtual.client.hook.patchs.am;
 
 import android.app.IApplicationThread;
 import android.app.IServiceConnection;
+import android.content.ComponentName;
 import android.content.Intent;
 import android.content.pm.ServiceInfo;
+import android.os.Build;
 import android.os.IBinder;
 
 import com.lody.virtual.client.core.VirtualCore;
@@ -14,40 +16,43 @@ import java.lang.reflect.Method;
 
 /**
  * @author Lody
- *
- *         <p/>
+ *         <p>
+ *         <p>
  *         原型: public int bindService(IApplicationThread caller, IBinder token,
  *         Intent service, String resolvedType, IServiceConnection connection,
  *         int flags, String callingPackage, int userId)
  */
 /* package */ class Hook_BindService extends Hook {
 
-	@Override
-	public String getName() {
-		return "bindService";
-	}
+  @Override
+  public String getName() {
+    return "bindService";
+  }
 
-	@Override
-	public Object onHook(Object who, Method method, Object... args) throws Throwable {
-		IApplicationThread caller = (IApplicationThread) args[0];
-		IBinder token = (IBinder) args[1];
-		Intent service = (Intent) args[2];
-		String resolvedType = (String) args[3];
-		IServiceConnection connection = (IServiceConnection) args[4];
-		int flags = (int) args[5];
-		ServiceInfo serviceInfo = VirtualCore.getCore().resolveServiceInfo(service);
-		if (serviceInfo != null) {
-			String pkgName = serviceInfo.packageName;
-			if (isAppPkg(pkgName)) {
-				return LocalServiceManager.getInstance().bindService(caller.asBinder(), token, service, resolvedType,
-						connection, flags);
-			}
-		}
-		return method.invoke(who, args);
-	}
+  @Override
+  public Object onHook(Object who, Method method, Object... args) throws Throwable {
+    IApplicationThread caller = (IApplicationThread) args[0];
+    IBinder token = (IBinder) args[1];
+    Intent service = (Intent) args[2];
+    String resolvedType = (String) args[3];
+    IServiceConnection connection = (IServiceConnection) args[4];
+    int flags = (int) args[5];
+    ServiceInfo serviceInfo = VirtualCore.getCore().resolveServiceInfo(service);
+    if (serviceInfo != null) {
+      String pkgName = serviceInfo.packageName;
+//      if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.L ) {
+//        service.setComponent(new ComponentName(serviceInfo.packageName, serviceInfo.name));
+//      }
+      if (isAppPkg(pkgName)) {
+        return LocalServiceManager.getInstance().bindService(caller.asBinder(), token, service, resolvedType,
+                connection, flags);
+      }
+    }
+    return method.invoke(who, args);
+  }
 
-	@Override
-	public boolean isEnable() {
-		return isAppProcess() || isServiceProcess();
-	}
+  @Override
+  public boolean isEnable() {
+    return isAppProcess() || isServiceProcess();
+  }
 }
