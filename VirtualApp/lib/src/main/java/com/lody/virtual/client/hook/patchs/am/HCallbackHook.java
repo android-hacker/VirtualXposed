9fac5d0c2d5ab396a32c98be97fadfa8e0c1188f
qiang.sheng <qiang.sheng@godinsec.com>
2016-07-26 22:16:30 +0800

    fix : 在使用Application的startActivity方法时因为token为空不能启动的问题

--- VirtualApp/lib/src/main/java/com/lody/virtual/client/hook/patchs/am/HCallbackHook.java
+++ VirtualApp/lib/src/main/java/com/lody/virtual/client/hook/patchs/am/HCallbackHook.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Build;
 import android.os.Handler;
+import android.os.IBinder;
 import android.os.Message;
 
 import com.lody.virtual.client.core.AppSandBox;
@@ -16,13 +17,14 @@ import com.lody.virtual.helper.compat.ActivityRecordCompat;
 import com.lody.virtual.helper.compat.ClassLoaderCompat;
 import com.lody.virtual.helper.proto.AppInfo;
 import com.lody.virtual.helper.utils.ComponentUtils;
+import com.lody.virtual.helper.utils.Reflect;
 import com.lody.virtual.helper.utils.VLog;
 
 import java.lang.reflect.Field;
 
 /**
  * @author Lody
- *
+ *         <p>
  *         <p>
  *         注入我们的Callback到
  *         <h1>ActivityThread$H</h1>.
@@ -31,144 +33,149 @@ import java.lang.reflect.Field;
  */
 public class HCallbackHook implements Handler.Callback, Injectable {
 
-	public static final int LAUNCH_ACTIVITY = 100;
+    public static final int LAUNCH_ACTIVITY = 100;
 
-	private static final String TAG = HCallbackHook.class.getSimpleName();
-	private static final HCallbackHook sCallback = new HCallbackHook();
-	private static Field f_h;
-	private static Field f_handleCallback;
+    private static final String TAG = HCallbackHook.class.getSimpleName();
+    private static final HCallbackHook sCallback = new HCallbackHook();
+    private static Field f_h;
+    private static Field f_handleCallback;
 
-	private boolean calling = false;
+    private boolean calling = false;
 
-	static {
-		try {
-			f_h = ActivityThread.class.getDeclaredField("mH");
-			f_handleCallback = Handler.class.getDeclaredField("mCallback");
-			f_h.setAccessible(true);
-			f_handleCallback.setAccessible(true);
-		} catch (NoSuchFieldException e) {
-			// Ignore
-		}
-	}
+    static {
+        try {
+            f_h = ActivityThread.class.getDeclaredField("mH");
+            f_handleCallback = Handler.class.getDeclaredField("mCallback");
+            f_h.setAccessible(true);
+            f_handleCallback.setAccessible(true);
+        } catch (NoSuchFieldException e) {
+            // Ignore
+        }
+    }
 
-	/**
-	 * 其它插件化可能也会注入Activity$H, 这里要保留其它插件化的Callback引用，我们的Callback完事后再调用它的。
-	 */
-	private Handler.Callback otherCallback;
+    /**
+     * 其它插件化可能也会注入Activity$H, 这里要保留其它插件化的Callback引用，我们的Callback完事后再调用它的。
+     */
+    private Handler.Callback otherCallback;
 
-	private HCallbackHook() {
-	}
+    private HCallbackHook() {
+    }
 
-	public static HCallbackHook getDefault() {
-		return sCallback;
-	}
+    public static HCallbackHook getDefault() {
+        return sCallback;
+    }
 
-	public static Handler getH() {
-		try {
-			return (Handler) f_h.get(VirtualCore.mainThread());
-		} catch (Throwable e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
+    public static Handler getH() {
+        try {
+            return (Handler) f_h.get(VirtualCore.mainThread());
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
-	private static Handler.Callback getHCallback() {
-		try {
-			Handler handler = getH();
-			return (Handler.Callback) f_handleCallback.get(handler);
-		} catch (Throwable e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
+    private static Handler.Callback getHCallback() {
+        try {
+            Handler handler = getH();
+            return (Handler.Callback) f_handleCallback.get(handler);
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
-	@Override
-	public boolean handleMessage(Message msg) {
-		if (!calling) {
-			calling = true;
-			try {
-				if (LAUNCH_ACTIVITY == msg.what) {
-					handleLaunchActivity(msg);
-				}
-				if (otherCallback != null) {
-					return otherCallback.handleMessage(msg);
-				}
-			} finally {
-				calling = false;
-			}
-		}
-		return false;
-	}
+    @Override
+    public boolean handleMessage(Message msg) {
+        if (!calling) {
+            calling = true;
+            try {
+                if (LAUNCH_ACTIVITY == msg.what) {
+                    handleLaunchActivity(msg);
+                }
+                if (otherCallback != null) {
+                    return otherCallback.handleMessage(msg);
+                }
+            } finally {
+                calling = false;
+            }
+        }
+        return false;
+    }
 
-	private void handleLaunchActivity(Message msg) {
-		Object r = msg.obj;
-		// StubIntent
-		Intent stubIntent = ActivityRecordCompat.getIntent(r);
-		// TargetIntent
-		Intent targetIntent = stubIntent.getParcelableExtra(ExtraConstants.EXTRA_TARGET_INTENT);
+    public static IBinder token = null;
 
-		ComponentName component = targetIntent.getComponent();
-		String pkgName = component.getPackageName();
+    private void handleLaunchActivity(Message msg) {
+        if (token == null) {
+            token = Reflect.on(msg.obj).get("token");
+        }
+        Object r = msg.obj;
+        // StubIntent
+        Intent stubIntent = ActivityRecordCompat.getIntent(r);
+        // TargetIntent
+        Intent targetIntent = stubIntent.getParcelableExtra(ExtraConstants.EXTRA_TARGET_INTENT);
 
-		AppInfo appInfo = VirtualCore.getCore().findApp(pkgName);
+        ComponentName component = targetIntent.getComponent();
+        String pkgName = component.getPackageName();
 
-		if (appInfo == null) {
-			return;
-		}
-		// StubActivityInfo
-		ActivityInfo stubActInfo = stubIntent.getParcelableExtra(ExtraConstants.EXTRA_STUB_ACT_INFO);
-		// TargetActivityInfo
-		ActivityInfo targetActInfo = stubIntent.getParcelableExtra(ExtraConstants.EXTRA_TARGET_ACT_INFO);
+        AppInfo appInfo = VirtualCore.getCore().findApp(pkgName);
 
-		if (stubActInfo == null || targetActInfo == null) {
-			return;
-		}
-		String processName = ComponentUtils.getProcessName(targetActInfo);
-		AppSandBox.install(processName, targetActInfo.packageName);
+        if (appInfo == null) {
+            return;
+        }
+        // StubActivityInfo
+        ActivityInfo stubActInfo = stubIntent.getParcelableExtra(ExtraConstants.EXTRA_STUB_ACT_INFO);
+        // TargetActivityInfo
+        ActivityInfo targetActInfo = stubIntent.getParcelableExtra(ExtraConstants.EXTRA_TARGET_ACT_INFO);
 
-		ClassLoader pluginClassLoader = appInfo.getClassLoader();
+        if (stubActInfo == null || targetActInfo == null) {
+            return;
+        }
+        String processName = ComponentUtils.getProcessName(targetActInfo);
+        AppSandBox.install(processName, targetActInfo.packageName);
 
-		targetIntent.setExtrasClassLoader(pluginClassLoader);
+        ClassLoader pluginClassLoader = appInfo.getClassLoader();
 
-		boolean error = false;
-		try {
-			targetIntent.putExtra(ExtraConstants.EXTRA_STUB_ACT_INFO, stubActInfo);
-			targetIntent.putExtra(ExtraConstants.EXTRA_TARGET_ACT_INFO, targetActInfo);
-		} catch (Throwable e) {
-			error = true;
-			VLog.w(TAG, "Directly putExtra failed: %s.", e.getMessage());
-		}
-		if (error && Build.VERSION.SDK_INT <= Build.VERSION_CODES.KITKAT) {
-			// 4.4以下的设备会出现这个BUG(unParcel找不到类加载器),
-			// 只能通过注入Class.forName所使用的类加载器来解决了...
-			ClassLoader oldParent = ClassLoaderCompat.setParent(getClass().getClassLoader(), pluginClassLoader);
-			try {
-				targetIntent.putExtra(ExtraConstants.EXTRA_STUB_ACT_INFO, stubActInfo);
-				targetIntent.putExtra(ExtraConstants.EXTRA_TARGET_ACT_INFO, targetActInfo);
-			} catch (Throwable e) {
-				VLog.w(TAG, "Secondly putExtra failed: %s.", e.getMessage());
-			}
-			ClassLoaderCompat.setParent(getClass().getClassLoader(), oldParent);
-		}
-		ActivityRecordCompat.setIntent(r, targetIntent);
-		ActivityRecordCompat.setActivityInfo(r, targetActInfo);
-	}
+        targetIntent.setExtrasClassLoader(pluginClassLoader);
+
+        boolean error = false;
+        try {
+            targetIntent.putExtra(ExtraConstants.EXTRA_STUB_ACT_INFO, stubActInfo);
+            targetIntent.putExtra(ExtraConstants.EXTRA_TARGET_ACT_INFO, targetActInfo);
+        } catch (Throwable e) {
+            error = true;
+            VLog.w(TAG, "Directly putExtra failed: %s.", e.getMessage());
+        }
+        if (error && Build.VERSION.SDK_INT <= Build.VERSION_CODES.KITKAT) {
+            // 4.4以下的设备会出现这个BUG(unParcel找不到类加载器),
+            // 只能通过注入Class.forName所使用的类加载器来解决了...
+            ClassLoader oldParent = ClassLoaderCompat.setParent(getClass().getClassLoader(), pluginClassLoader);
+            try {
+                targetIntent.putExtra(ExtraConstants.EXTRA_STUB_ACT_INFO, stubActInfo);
+                targetIntent.putExtra(ExtraConstants.EXTRA_TARGET_ACT_INFO, targetActInfo);
+            } catch (Throwable e) {
+                VLog.w(TAG, "Secondly putExtra failed: %s.", e.getMessage());
+            }
+            ClassLoaderCompat.setParent(getClass().getClassLoader(), oldParent);
+        }
+        ActivityRecordCompat.setIntent(r, targetIntent);
+        ActivityRecordCompat.setActivityInfo(r, targetActInfo);
+    }
 
 
-	@Override
-	public void inject() throws Throwable {
-		otherCallback = getHCallback();
-		f_handleCallback.set(getH(), this);
-	}
+    @Override
+    public void inject() throws Throwable {
+        otherCallback = getHCallback();
+        f_handleCallback.set(getH(), this);
+    }
 
-	@Override
-	public boolean isEnvBad() {
-		Handler.Callback callback = getHCallback();
-		boolean envBad = callback != this;
-		if (envBad) {
-			VLog.d(TAG, "HCallback has bad, other callback = " + callback);
-		}
-		return envBad;
-	}
+    @Override
+    public boolean isEnvBad() {
+        Handler.Callback callback = getHCallback();
+        boolean envBad = callback != this;
+        if (envBad) {
+            VLog.d(TAG, "HCallback has bad, other callback = " + callback);
+        }
+        return envBad;
+    }
 
 }
