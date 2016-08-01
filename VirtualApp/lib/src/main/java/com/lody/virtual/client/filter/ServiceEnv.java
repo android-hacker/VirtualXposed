8d2984babe88cef54e19ed73a5b2060b2e013779
qiang.sheng <qiang.sheng@godinsec.com>
2016-08-01 17:20:58 +0800

    Feature: 添加Intent过滤服务，用以拦截应用安装卸载等Intent,为跳转到自己写的安装卸载服务做准备。

---
+++ VirtualApp/lib/src/main/java/com/lody/virtual/client/filter/ServiceEnv.java
@@ -0,0 +1,10 @@
+package com.lody.virtual.client.filter;
+
+/**
+ * Class:
+ * Created by andy on 16-8-1.
+ * TODO:
+ */
+public class ServiceEnv {
+  public static String IntentFilter = "IntentFilter";
+}
