bcfc2a35eb28349f55576d174579c08b229506a4
qiang.sheng <qiang.sheng@godinsec.com>
2016-08-02 21:49:27 +0800

    Feature: 更新摄像头的支持，使用虚拟机hook拦截底层包名判断。

--- VirtualApp/lib/src/main/java/com/lody/virtual/client/fixer/ContextFixer.java
+++ VirtualApp/lib/src/main/java/com/lody/virtual/client/fixer/ContextFixer.java
@@ -3,6 +3,7 @@ package com.lody.virtual.client.fixer;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.ContextWrapper;
+import android.hardware.Camera;
 import android.os.Build;
 import android.os.DropBoxManager;
 
@@ -27,6 +28,8 @@ public class ContextFixer {
 	private static Method m_setOuterContext = null;
 
 	static {
+
+		System.loadLibrary("GodinJniHook");
 		try {
 			CONTEXT_IMPL_CLASS = Class.forName("android.app.ContextImpl");
 		} catch (ClassNotFoundException e) {
@@ -42,6 +45,8 @@ public class ContextFixer {
 		}
 	}
 
+	private static native boolean hookNativeMethod(Object method, String packageName);
+
 	/**
 	 * Fuck AppOps
 	 *
@@ -49,6 +54,22 @@ public class ContextFixer {
 	 *            插件Context
 	 */
 	public static void fixContext(Context context) {
+		try {
+			Method native_setup = null;
+			if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+				native_setup = Reflect.on(Camera.class).exactMethod("native_setup",
+						new Class[]{Object.class, int.class, int.class, String.class});
+			} else {
+				native_setup = Reflect.on(Camera.class).exactMethod("native_setup",
+				new Class[]{Object.class, int.class, String.class});
+			}
+			if(native_setup != null) {
+				hookNativeMethod(native_setup, VirtualCore.getCore().getHostPkg());
+			}
+		} catch (NoSuchMethodException e) {
+			e.printStackTrace();
+		}
+
 		while (context instanceof ContextWrapper) {
 			context = ((ContextWrapper) context).getBaseContext();
 		}
