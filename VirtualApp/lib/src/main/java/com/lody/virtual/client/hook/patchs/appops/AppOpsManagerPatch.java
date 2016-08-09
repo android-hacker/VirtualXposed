937c0e20c2193e9d1c955ddaa118c10cf47ff122
asLody <imlody@foxmail.com>
2016-08-09 12:38:11 +0800

    Fix: AppOps crash.

--- VirtualApp/lib/src/main/java/com/lody/virtual/client/hook/patchs/appops/AppOpsManagerPatch.java
+++ VirtualApp/lib/src/main/java/com/lody/virtual/client/hook/patchs/appops/AppOpsManagerPatch.java
@@ -1,14 +1,19 @@
 package com.lody.virtual.client.hook.patchs.appops;
 
-import com.android.internal.app.IAppOpsService;
-import com.lody.virtual.client.hook.base.Patch;
-import com.lody.virtual.client.hook.base.PatchObject;
-import com.lody.virtual.client.hook.binders.HookAppOpsBinder;
-
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.os.Build;
+import android.os.Process;
 import android.os.ServiceManager;
+
+import com.android.internal.app.IAppOpsService;
+import com.lody.virtual.client.hook.base.PatchObject;
+import com.lody.virtual.client.hook.base.ReplaceCallingPkgHook;
+import com.lody.virtual.client.hook.base.ReplaceLastPkgHook;
+import com.lody.virtual.client.hook.base.StaticHook;
+import com.lody.virtual.client.hook.binders.HookAppOpsBinder;
+
+import java.lang.reflect.Method;
 
 /**
  * @author Lody
@@ -20,9 +25,6 @@ import android.os.ServiceManager;
  * @see android.app.AppOpsManager
  * @see IAppOpsService
  */
-@Patch({Hook_CheckAudioOperation.class, Hook_CheckOperation.class, Hook_CheckPackage.class, Hook_FinishOperation.class,
-		Hook_GetOpsForPackage.class, Hook_NoteOperation.class, Hook_SetMode.class, Hook_StartOperation.class,
-		Hook_StartWatchingMode.class, Hook_NoteProxyOperation.class, Hook_ResetAllModes.class,})
 @TargetApi(Build.VERSION_CODES.KITKAT)
 public class AppOpsManagerPatch extends PatchObject<IAppOpsService, HookAppOpsBinder> {
 
@@ -34,6 +36,48 @@ public class AppOpsManagerPatch extends PatchObject<IAppOpsService, HookAppOpsBi
 	@Override
 	public void inject() throws Throwable {
 		getHookObject().injectService(Context.APP_OPS_SERVICE);
+	}
+
+	@Override
+	protected void applyHooks() {
+		super.applyHooks();
+		addHook(new BaseHook("checkOperation", 1, 2));
+		addHook(new BaseHook("noteOperation", 1, 2));
+		addHook(new BaseHook("startOperation", 2, 3));
+		addHook(new BaseHook("finishOperation", 2, 3));
+		addHook(new BaseHook("startWatchingMode", -1, 1));
+		addHook(new BaseHook("checkPackage", 0, 1));
+		addHook(new BaseHook("checkPackage", 0, 1));
+		addHook(new BaseHook("getOpsForPackage", 0, 1));
+		addHook(new BaseHook("setMode", 1, 2));
+		addHook(new BaseHook("checkAudioOperation", 2, 3));
+		addHook(new BaseHook("setAudioRestriction", 2, -1));
+		addHook(new ReplaceCallingPkgHook("noteProxyOperation"));
+		addHook(new ReplaceLastPkgHook("resetAllModes"));
+	}
+
+	private class BaseHook extends StaticHook {
+		final int pkgIndex;
+		final int uidIndex;
+		public BaseHook(String name, int pkgIndex, int uidIndex) {
+			super(name);
+			this.pkgIndex = pkgIndex;
+			this.uidIndex = uidIndex;
+		}
+
+		@Override
+		public boolean beforeHook(Object who, Method method, Object... args) {
+			if (pkgIndex != -1 && args.length > pkgIndex && args[pkgIndex] instanceof String) {
+				String pkg = (String) args[pkgIndex];
+				if (isAppPkg(pkg)) {
+					args[pkgIndex] = getHostPkg();
+				}
+			}
+			if (uidIndex != -1 && args.length > uidIndex && args[uidIndex] instanceof Integer) {
+				args[uidIndex] = Process.myUid();
+			}
+			return true;
+		}
 	}
 
 	@Override
