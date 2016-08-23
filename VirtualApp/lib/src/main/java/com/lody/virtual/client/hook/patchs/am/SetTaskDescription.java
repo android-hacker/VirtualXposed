cf5bf7d544225a035c2792cb710a9f8019e434fd
prife <goprife@gmail.com>
2016-08-23 11:13:58 +0800

    Fix: some app's label is virtapp in Overview Screen
    if the client call setTaskDescription with (label=null, icon=null), then
    the label and icon will be VirtuallApp in Overview Screen.
    
    TestCase: Solid Explorer classic

--- VirtualApp/lib/src/main/java/com/lody/virtual/client/hook/patchs/am/SetTaskDescription.java
+++ VirtualApp/lib/src/main/java/com/lody/virtual/client/hook/patchs/am/SetTaskDescription.java
@@ -3,6 +3,9 @@ package com.lody.virtual.client.hook.patchs.am;
 import android.annotation.TargetApi;
 import android.app.ActivityManager;
 import android.app.Application;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.IBinder;
 
@@ -10,7 +13,6 @@ import com.lody.virtual.client.VClientImpl;
 import com.lody.virtual.client.hook.base.Hook;
 
 import java.lang.reflect.Method;
-
 
 /**
  * @author prife
@@ -30,11 +32,23 @@ import java.lang.reflect.Method;
 		ActivityManager.TaskDescription td = (ActivityManager.TaskDescription)args[1];
 
 		String label = td.getLabel();
-		if (label == null) {
+		Bitmap icon = td.getIcon();
+		if (label == null || icon == null) {
 			Application app = VClientImpl.getClient().getCurrentApplication();
-			label = app.getApplicationInfo().loadLabel(app.getPackageManager()) + VACLIENT_SUFFIX;
-			td.setLabel(label);
-		} else if (label != null && !label.endsWith(VACLIENT_SUFFIX)) {
+			if (label == null) {
+				label = "" + app.getApplicationInfo().loadLabel(app.getPackageManager());
+			}
+
+			if (icon == null) {
+				Drawable drawable = app.getApplicationInfo().loadIcon(app.getPackageManager());
+				if (drawable instanceof BitmapDrawable) {
+					icon = ((BitmapDrawable) drawable).getBitmap();
+					td.setIcon(icon);
+				}
+			}
+		}
+
+		if (!label.endsWith(VACLIENT_SUFFIX)) {
 			td.setLabel(label + VACLIENT_SUFFIX);
 		}
 
