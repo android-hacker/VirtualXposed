423eb772f34145a122ab17e65618df179f750889
keyongyu <247321453@qq.com>
2016-10-23 14:46:30 +0800

    录音

--- VirtualApp/lib/src/main/java/com/lody/virtual/client/hook/patchs/vibrator/VibratorPatch.java
+++ VirtualApp/lib/src/main/java/com/lody/virtual/client/hook/patchs/vibrator/VibratorPatch.java
@@ -12,37 +12,45 @@ import mirror.android.os.ServiceManager;
 
 /**
  * @author Lody
- *
- *
  * @see android.os.Vibrator
  */
 public class VibratorPatch extends PatchDelegate<VibratorBinderDelegate> {
 
-	@Override
-	protected VibratorBinderDelegate createHookDelegate() {
-		return new VibratorBinderDelegate();
-	}
+    @Override
+    protected VibratorBinderDelegate createHookDelegate() {
+        return new VibratorBinderDelegate();
+    }
 
-	@Override
-	public void inject() throws Throwable {
-		getHookDelegate().replaceService(Context.VIBRATOR_SERVICE);
-	}
+    @Override
+    public void inject() throws Throwable {
+        getHookDelegate().replaceService(Context.VIBRATOR_SERVICE);
+    }
 
-	@Override
-	protected void onBindHooks() {
-		addHook(new ReplaceCallingPkgHook("vibrate"));
-		addHook(new ReplaceCallingPkgHook("vibratePattern") {
-			@Override
-			public boolean beforeCall(Object who, Method method, Object... args) {
-				args[0] = getRealUid();
-				return super.beforeCall(who, method, args);
-			}
-		});
-	}
+    @Override
+    protected void onBindHooks() {
+        addHook(new ReplaceCallingPkgHook("vibrate") {
+            @Override
+            public boolean beforeCall(Object who, Method method, Object... args) {
+                if (args[0] instanceof Integer) {
+                    args[0] = getRealUid();
+                }
+                return super.beforeCall(who, method, args);
+            }
+        });
+        addHook(new ReplaceCallingPkgHook("vibratePattern") {
+            @Override
+            public boolean beforeCall(Object who, Method method, Object... args) {
+                if (args[0] instanceof Integer) {
+                    args[0] = getRealUid();
+                }
+                return super.beforeCall(who, method, args);
+            }
+        });
+    }
 
-	@Override
-	public boolean isEnvBad() {
-		return getHookDelegate() != ServiceManager.getService.call(Context.VIBRATOR_SERVICE);
-	}
+    @Override
+    public boolean isEnvBad() {
+        return getHookDelegate() != ServiceManager.getService.call(Context.VIBRATOR_SERVICE);
+    }
 
 }
