bf3dd532a0e2350261c17d5a4c0f87ce2b9f3032
Lody <imlody@foxmail.com>
2017-01-16 10:12:38 +0800

    Fix: getIntentSender bug.

--- VirtualApp/lib/src/main/java/com/lody/virtual/client/hook/patchs/am/GetIntentSender.java
+++ VirtualApp/lib/src/main/java/com/lody/virtual/client/hook/patchs/am/GetIntentSender.java
@@ -21,88 +21,90 @@ import java.lang.reflect.Method;
  */
 /* package */ class GetIntentSender extends BaseStartActivity {
 
-	@Override
-	public String getName() {
-		return "getIntentSender";
-	}
+    @Override
+    public String getName() {
+        return "getIntentSender";
+    }
 
-	@Override
-	public Object call(Object who, Method method, Object... args) throws Throwable {
-		String creator = (String) args[1];
-		args[1] = getHostPkg();
-		String[] resolvedTypes = (String[]) args[6];
-		int type = (int) args[0];
-		if (args[5] instanceof Intent[]) {
-			Intent[] intents = (Intent[]) args[5];
-			if (intents.length > 0) {
-				Intent intent = intents[intents.length - 1];
-				if (resolvedTypes != null && resolvedTypes.length > 0) {
-					intent.setDataAndType(intent.getData(), resolvedTypes[resolvedTypes.length - 1]);
-				}
-				Intent proxyIntent = redirectIntentSender(type, creator, intent);
-				if (proxyIntent != null) {
-					intents[intents.length - 1] = proxyIntent;
-				}
-			}
-		}
-		if (args.length > 7 && args[7] instanceof Integer) {
+    @Override
+    public Object call(Object who, Method method, Object... args) throws Throwable {
+        String creator = (String) args[1];
+        args[1] = getHostPkg();
+        String[] resolvedTypes = (String[]) args[6];
+        int type = (int) args[0];
+        if (args[5] instanceof Intent[]) {
+            Intent[] intents = (Intent[]) args[5];
+            if (intents.length > 0) {
+                Intent intent = intents[intents.length - 1];
+                if (resolvedTypes != null && resolvedTypes.length > 0) {
+                    intent.setDataAndType(intent.getData(), resolvedTypes[resolvedTypes.length - 1]);
+                }
+                Intent proxyIntent = redirectIntentSender(type, creator, intent);
+                if (proxyIntent != null) {
+                    intents[intents.length - 1] = proxyIntent;
+                }
+            }
+        }
+        if (args.length > 7 && args[7] instanceof Integer) {
             args[7] = PendingIntent.FLAG_UPDATE_CURRENT;
         }
-		args[6] = new String[] {null};
-		IInterface sender = (IInterface) method.invoke(who, args);
-		if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR2 && sender != null && creator != null) {
-			VActivityManager.get().addPendingIntent(sender.asBinder(), creator);
-		}
-		return sender;
-	}
+        IInterface sender = (IInterface) method.invoke(who, args);
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR2 && sender != null && creator != null) {
+            VActivityManager.get().addPendingIntent(sender.asBinder(), creator);
+        }
+        return sender;
+    }
 
-	private Intent redirectIntentSender(int type, String creator, Intent intent) {
-		Intent newIntent = intent.cloneFilter();
-		boolean ok = false;
+    private Intent redirectIntentSender(int type, String creator, Intent intent) {
+        Intent newIntent = intent.cloneFilter();
+        boolean ok = false;
 
-		switch (type) {
-			case ActivityManagerCompat.INTENT_SENDER_ACTIVITY: {
+        switch (type) {
+            case ActivityManagerCompat.INTENT_SENDER_ACTIVITY: {
 
-				ComponentInfo info = VirtualCore.get().resolveActivityInfo(intent, VUserHandle.myUserId());
-				if (info != null) {
-					ok = true;
-					newIntent.setClass(getHostContext(), StubPendingActivity.class);
-					newIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-				}
+                ComponentInfo info = VirtualCore.get().resolveActivityInfo(intent, VUserHandle.myUserId());
+                if (info != null) {
+                    ok = true;
+                    newIntent.setClass(getHostContext(), StubPendingActivity.class);
+                    newIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                }
 
-			} break;
+            }
+            break;
 
-			case ActivityManagerCompat.INTENT_SENDER_SERVICE: {
-				ComponentInfo info = VirtualCore.get().resolveServiceInfo(intent, VUserHandle.myUserId());
-				if (info != null) {
-					ok= true;
-					newIntent.setClass(getHostContext(), StubPendingService.class);
-				}
+            case ActivityManagerCompat.INTENT_SENDER_SERVICE: {
+                ComponentInfo info = VirtualCore.get().resolveServiceInfo(intent, VUserHandle.myUserId());
+                if (info != null) {
+                    ok = true;
+                    newIntent.setClass(getHostContext(), StubPendingService.class);
+                }
 
-			} break;
+            }
+            break;
 
-			case ActivityManagerCompat.INTENT_SENDER_BROADCAST: {
-				ok = true;
-				newIntent.setClass(getHostContext(), StubPendingReceiver.class);
-			} break;
+            case ActivityManagerCompat.INTENT_SENDER_BROADCAST: {
+                ok = true;
+                newIntent.setClass(getHostContext(), StubPendingReceiver.class);
+            }
+            break;
 
-		}
+        }
 
-		if (!ok) {
-			return null;
-		}
+        if (!ok) {
+            return null;
+        }
 
-		newIntent.putExtra("_VA_|_user_id_", VUserHandle.myUserId());
-		newIntent.putExtra("_VA_|_intent_", intent);
-		newIntent.putExtra("_VA_|_creator_", creator);
-		newIntent.putExtra("_VA_|_from_inner_", true);
+        newIntent.putExtra("_VA_|_user_id_", VUserHandle.myUserId());
+        newIntent.putExtra("_VA_|_intent_", intent);
+        newIntent.putExtra("_VA_|_creator_", creator);
+        newIntent.putExtra("_VA_|_from_inner_", true);
 
-		return newIntent;
-	}
+        return newIntent;
+    }
 
-	@Override
-	public boolean isEnable() {
-		return isAppProcess();
-	}
+    @Override
+    public boolean isEnable() {
+        return isAppProcess();
+    }
 
 }
