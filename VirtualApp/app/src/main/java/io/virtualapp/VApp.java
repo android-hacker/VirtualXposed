1534a94d8ab75b47e9346c595506989b0e25c762
Lody <imlody@foxmail.com>
2017-01-26 20:27:32 +0800

    F

--- VirtualApp/app/src/main/java/io/virtualapp/VApp.java
+++ VirtualApp/app/src/main/java/io/virtualapp/VApp.java
@@ -2,15 +2,9 @@ package io.virtualapp;
 
 import android.app.Application;
 import android.content.Context;
-import android.content.pm.ApplicationInfo;
-import android.content.pm.PackageManager;
 
-import com.lody.virtual.client.core.InstallStrategy;
 import com.lody.virtual.client.core.VirtualCore;
-import com.lody.virtual.client.ipc.ServiceManagerNative;
 import com.lody.virtual.client.stub.StubManifest;
-import com.lody.virtual.helper.proto.InstallResult;
-import com.lody.virtual.helper.utils.VLog;
 
 import jonathanfinerty.once.Once;
 
@@ -30,7 +24,7 @@ public class VApp extends Application {
     protected void attachBaseContext(Context base) {
         super.attachBaseContext(base);
         try {
-            StubManifest.ENABLE_IO_REDIRECT = false;
+            StubManifest.ENABLE_IO_REDIRECT = true;
             VirtualCore.get().startup(base);
         } catch (Throwable e) {
             e.printStackTrace();
