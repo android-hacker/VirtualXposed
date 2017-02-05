6049f99b3c2770247b3e76b6a519f46f0fd2f8ca
CodeHz <CodeHz@Outlook.com>
2017-02-05 21:28:27 +0800

    Clear WTF import...

--- VirtualApp/lib/src/main/java/mirror/webkit/IWebViewUpdateService.java
+++ VirtualApp/lib/src/main/java/mirror/webkit/IWebViewUpdateService.java
@@ -1,13 +1,7 @@
 package mirror.webkit;
 
-import android.os.IBinder;
-import android.os.IInterface;
-
-import mirror.MethodParams;
 import mirror.RefClass;
 import mirror.RefMethod;
-import mirror.RefStaticMethod;
-import mirror.android.net.wifi.IWifiManager;
 
 /**
  * @author CodeHz
