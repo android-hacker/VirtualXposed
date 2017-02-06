51c9aab7304fea412d20dc435268b019529596f3
Lody <imlody@foxmail.com>
2017-02-06 18:16:26 +0800

    Code: enable the IO Redirect by default.

--- VirtualApp/lib/src/main/java/com/lody/virtual/client/stub/StubManifest.java
+++ VirtualApp/lib/src/main/java/com/lody/virtual/client/stub/StubManifest.java
@@ -7,11 +7,11 @@ import java.util.Locale;
  */
 
 public class StubManifest {
+    public static final String STUB_DEF_AUTHORITY = "virtual_stub_";
     public static String STUB_ACTIVITY = StubActivity.class.getName();
     public static String STUB_DIALOG = StubDialog.class.getName();
     public static String STUB_CP = StubContentProvider.class.getName();
     public static String STUB_JOB = StubJob.class.getName();
-    public static final String STUB_DEF_AUTHORITY = "virtual_stub_";
     public static String STUB_CP_AUTHORITY = "virtual_stub_";
     public static int STUB_COUNT = 50;
 
@@ -28,7 +28,7 @@ public class StubManifest {
      * when app access '/data/data/{Package Name}' or '/data/user/0/{Package Name}',
      * we redirect it to '/data/data/{Your Host Package Name}/virtual/user/0/{Package Name}'.
      */
-    public static boolean ENABLE_IO_REDIRECT = false;
+    public static boolean ENABLE_IO_REDIRECT = true;
 
     public static String getStubActivityName(int index) {
         return String.format(Locale.ENGLISH, "%s$C%d", STUB_ACTIVITY, index);
