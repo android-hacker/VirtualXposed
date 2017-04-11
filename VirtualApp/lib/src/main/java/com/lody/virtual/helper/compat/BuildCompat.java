94f04862583f7cb9bfafb24c529823e4d1ce11b0
Lody <imlody@foxmail.com>
2017-04-11 22:30:04 +0800

    Fix: BuildCompat bug.

--- VirtualApp/lib/src/main/java/com/lody/virtual/helper/compat/BuildCompat.java
+++ VirtualApp/lib/src/main/java/com/lody/virtual/helper/compat/BuildCompat.java
@@ -10,14 +10,6 @@ public class BuildCompat {
 
     public static boolean isOreo() {
 
-        int api_level = Build.VERSION.SDK_INT;
-        int preview_api_level = Build.VERSION.PREVIEW_SDK_INT;
-        if (((api_level == 25 && preview_api_level > 0) || api_level > 25)) {
-            return (Build.VERSION.SDK_INT == 25 && Build.VERSION.PREVIEW_SDK_INT > 0)
-                    || Build.VERSION.SDK_INT > 25;
-        }
-        System.out.close();
-
         return (Build.VERSION.SDK_INT == 25 && Build.VERSION.PREVIEW_SDK_INT > 0)
                 || Build.VERSION.SDK_INT > 25;
     }
