a0220f7cfd7cf2b92ef3ee020b9bef603f1144ca
asLody <imlody@foxmail.com>
2017-09-29 21:41:11 +0800

    Fix: ContextHub on Oreo.

--- VirtualApp/lib/src/main/java/com/lody/virtual/client/hook/proxies/context_hub/ContextHubServiceStub.java
+++ VirtualApp/lib/src/main/java/com/lody/virtual/client/hook/proxies/context_hub/ContextHubServiceStub.java
@@ -1,5 +1,7 @@
 package com.lody.virtual.client.hook.proxies.context_hub;
 
+import android.os.Build;
+
 import com.lody.virtual.client.hook.base.BinderInvocationProxy;
 import com.lody.virtual.client.hook.base.ResultStaticMethodProxy;
 
@@ -8,7 +10,11 @@ import mirror.android.hardware.location.IContextHubService;
 public class ContextHubServiceStub extends BinderInvocationProxy {
 
     public ContextHubServiceStub() {
-        super(IContextHubService.Stub.asInterface, "contexthub_service");
+        super(IContextHubService.Stub.asInterface, getServiceName());
+    }
+
+    private static String getServiceName() {
+        return Build.VERSION.SDK_INT >= 26 ? "contexthub" : "contexthub_service";
     }
 
     @Override
