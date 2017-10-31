a3c26ec200d6f18840028fb9717ee7c53f1638ee
asLody <imlody@foxmail.com>
2017-10-31 21:45:33 +0800

    [native-jni] : optimize the jni code.

--- VirtualApp/lib/src/main/jni/Jni/Helper.h
+++ VirtualApp/lib/src/main/jni/Jni/Helper.h
@@ -5,9 +5,27 @@
 #ifndef NDK_LOG_H
 #define NDK_LOG_H
 
-
-#define NELEM(x) ((int) (sizeof(x) / sizeof((x)[0])))
+#include <fb/include/fb/fbjni.h>
 
 #define NATIVE_METHOD(func_ptr, func_name, signature) { func_name, signature, reinterpret_cast<void*>(func_ptr) }
+
+class ScopeUtfString {
+public:
+    ScopeUtfString(jstring j_str) : _j_str(j_str),
+                                    _c_str(facebook::jni::Environment::current()->GetStringUTFChars(j_str, NULL)) {
+    }
+
+    const char *c_str() {
+        return _c_str;
+    }
+
+    ~ScopeUtfString() {
+        facebook::jni::Environment::current()->ReleaseStringUTFChars(_j_str, _c_str);
+    }
+
+private:
+    jstring _j_str;
+    const char *_c_str;
+};
 
 #endif //NDK_LOG_H
