16dab8d0c25eee1952b6952d1e90cbdf1068268e
tiann <923551233@qq.com>
2018-01-04 01:22:13 +0800

    [Exposed-UI] add auto update module.

---
+++ VirtualApp/app/src/main/java/io/virtualapp/update/VAVersionService.java
@@ -0,0 +1,68 @@
+package io.virtualapp.update;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.util.Log;
+
+import com.allenliu.versionchecklib.core.AVersionService;
+import com.allenliu.versionchecklib.core.AllenChecker;
+import com.allenliu.versionchecklib.core.VersionParams;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * @author weishu
+ * @date 18/1/4.
+ */
+
+public class VAVersionService extends AVersionService {
+    private static final String TAG = "VAVersionService";
+
+    public static final String CHECK_VERION_URL = "http://vaexposed.weishu.me/update.json";
+
+    @Override
+    public void onResponses(AVersionService service, String response) {
+        try {
+            JSONObject versionInfo = new JSONObject(response);
+//            {
+//                url: "https://raw.githubusercontent.com/feicien/android-auto-update/develop/extras/android-auto-update-v1.2.apk",
+//                        versionCode: 3,
+//                    updateMessage: "Android 7.0"
+//            }
+            String url = versionInfo.getString("url");
+            int versionCode = versionInfo.getInt("versionCode");
+            String updateMessage = versionInfo.getString("updateMessage");
+
+            int currentVersion = getCurrentVersionCode(this);
+            if (currentVersion != versionCode) {
+                showVersionDialog(url, "VAExposed 更新啦: ", updateMessage);
+            }
+        } catch (JSONException e) {
+            Log.e(TAG, "version info parse error!!", e);
+        }
+    }
+
+    public static void checkUpdate(Application context) {
+        VersionParams.Builder builder = new VersionParams.Builder()
+                .setRequestUrl(CHECK_VERION_URL)
+                .setShowDownloadingDialog(false)
+                .setService(VAVersionService.class);
+
+        AllenChecker.startVersionCheck(context, builder.build());
+    }
+
+    private static int getCurrentVersionCode(Context context) {
+        try {
+            // ---get the package info---
+            PackageManager pm = context.getPackageManager();
+            PackageInfo pi = pm.getPackageInfo(context.getPackageName(), 0);
+            return pi.versionCode;
+        } catch (Exception e) {
+            Log.e("VersionInfo", "Exception", e);
+        }
+        return -1;
+    }
+}
