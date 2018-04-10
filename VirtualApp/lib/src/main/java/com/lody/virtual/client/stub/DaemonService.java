da01b33385fbb82764c66394ccd694c650ac9370
weishu.tws <twsxtd@gmail.com>
2018-04-10 22:39:50 +0800

    [VXP]: sync launcher.

--- VirtualApp/lib/src/main/java/com/lody/virtual/client/stub/DaemonService.java
+++ VirtualApp/lib/src/main/java/com/lody/virtual/client/stub/DaemonService.java
@@ -5,6 +5,7 @@ import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
 
 import com.lody.virtual.client.core.VirtualCore;
 
