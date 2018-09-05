6ffd53f480402ac1df0a9dc4919c75c104700c73
tiann <923551233@qq.com>
2018-09-05 20:30:16 +0800

    [VA]: fix apache httpclient dependency.

--- VirtualApp/lib/src/main/java/com/lody/virtual/client/ipc/VPackageManager.java
+++ VirtualApp/lib/src/main/java/com/lody/virtual/client/ipc/VPackageManager.java
@@ -179,7 +179,25 @@ public class VPackageManager {
 
     public ApplicationInfo getApplicationInfo(String packageName, int flags, int userId) {
         try {
-            return getInterface().getApplicationInfo(packageName, flags, userId);
+            ApplicationInfo info = getInterface().getApplicationInfo(packageName, flags, userId);
+            if (info == null) {
+                return null;
+            }
+            final int P = 28;
+            final String APACHE_LEGACY = "/system/framework/org.apache.http.legacy.boot.jar";
+            if (android.os.Build.VERSION.SDK_INT >= P && info.targetSdkVersion < P) {
+                String[] newSharedLibraryFiles;
+                if (info.sharedLibraryFiles == null) {
+                    newSharedLibraryFiles = new String[]{APACHE_LEGACY};
+                } else {
+                    int newLength = info.sharedLibraryFiles.length + 1;
+                    newSharedLibraryFiles = new String[newLength];
+                    System.arraycopy(info.sharedLibraryFiles, 0, newSharedLibraryFiles, 0, newLength - 1);
+                    newSharedLibraryFiles[newLength - 1] = APACHE_LEGACY;
+                }
+                info.sharedLibraryFiles = newSharedLibraryFiles;
+            }
+            return info;
         } catch (RemoteException e) {
             return VirtualRuntime.crash(e);
         }
