a77f3461bcf3567ebf370b5e21b82b827b353bf1
tiann <923551233@qq.com>
2018-10-29 14:09:44 +0800

    Use c++_static instead of gunstl (NDK 18 doesn't support it now)

--- VirtualApp/lib/src/main/jni/Application.mk
+++ VirtualApp/lib/src/main/jni/Application.mk
@@ -1,6 +1,6 @@
 APP_ABI := armeabi armeabi-v7a
 APP_PLATFORM := android-14
-APP_STL := gnustl_static
+APP_STL := c++_static
 APP_OPTIM := release
 VA_ROOT          := $(call my-dir)
 NDK_MODULE_PATH  := $(NDK_MODULE_PATH):$(VA_ROOT)
