7d051f02b4d31daff09a841aa1baf62d7cd6b8f6
tiann <923551233@qq.com>
2018-10-29 14:44:23 +0800

    improved translation

--- VirtualApp/app/src/main/java/io/virtualapp/utils/Misc.java
+++ VirtualApp/app/src/main/java/io/virtualapp/utils/Misc.java
@@ -18,62 +18,42 @@ import moe.feng.alipay.zerosdk.AlipayZeroSdk;
  */
 public class Misc {
     public static void showDonate(Activity context) {
-        AlertDialog alertDialog = new AlertDialog.Builder(context, R.style.Theme_AppCompat_DayNight_Dialog_Alert)
-                .setTitle(R.string.donate_dialog_title)
-                .setMessage(R.string.donate_dialog_content)
-                .setPositiveButton(R.string.donate_dialog_yes, (dialog, which) -> {
-                    // show chooser dialog
+        final String alipay = context.getResources().getString(R.string.donate_alipay);
+        final String[] items = {alipay, "PayPal", "Bitcoin"};
 
-                    final String alipay = context.getResources().getString(R.string.donate_alipay);
-                    final String[] items = {alipay, "Paypal", "Bitcoin"};
+        AlertDialog chooseDialog = new AlertDialog.Builder(context, R.style.Theme_AppCompat_DayNight_Dialog_Alert)
+                .setTitle(R.string.donate_choose_title)
+                .setItems(items, (dialog1, which1) -> {
+                    dialog1.dismiss();
+                    if (which1 == 0) {
+                        if (!AlipayZeroSdk.hasInstalledAlipayClient(context)) {
+                            Toast.makeText(context, R.string.prompt_alipay_not_found, Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                        AlipayZeroSdk.startAlipayClient(context, "FKX016770URBZGZSR37U37");
+                    } else if (which1 == 1) {
+                        try {
+                            Intent t = new Intent(Intent.ACTION_VIEW);
+                            t.setData(Uri.parse("https://paypal.me/virtualxposed"));
+                            context.startActivity(t);
+                        } catch (Throwable ignored) {
+                            ignored.printStackTrace();
+                        }
+                    } else if (which1 == 2) {
+                        final String address = "39Wst8oL74pRP2vKPkPihH6RFQF4hWoBqU";
 
-                    AlertDialog chooseDialog = new AlertDialog.Builder(context, R.style.Theme_AppCompat_DayNight_Dialog_Alert)
-                            .setTitle(R.string.donate_choose_title)
-                            .setItems(items, (dialog1, which1) -> {
-                                dialog1.dismiss();
-                                if (which1 == 0) {
-                                    if (!AlipayZeroSdk.hasInstalledAlipayClient(context)) {
-                                        Toast.makeText(context, R.string.prompt_alipay_not_found, Toast.LENGTH_SHORT).show();
-                                        return;
-                                    }
-                                    AlipayZeroSdk.startAlipayClient(context, "FKX016770URBZGZSR37U37");
-                                } else if (which1 == 1) {
-                                    try {
-                                        Intent t = new Intent(Intent.ACTION_VIEW);
-                                        t.setData(Uri.parse("https://paypal.me/virtualxposed"));
-                                        context.startActivity(t);
-                                    } catch (Throwable ignored) {
-                                        ignored.printStackTrace();
-                                    }
-                                } else if (which1 == 2) {
-                                    final String address = "39Wst8oL74pRP2vKPkPihH6RFQF4hWoBqU";
-
-                                    try {
-                                        ClipboardManager clipboardManager = (ClipboardManager) context.getSystemService(Context.CLIPBOARD_SERVICE);
-                                        if (clipboardManager != null) {
-                                            clipboardManager.setPrimaryClip(ClipData.newPlainText(null, address));
-                                        }
-                                        Toast.makeText(context, context.getResources().getString(R.string.donate_bitconins_tips), Toast.LENGTH_SHORT).show();
-                                    } catch (Throwable ignored) {
-                                        ignored.printStackTrace();
-                                    }
-                                }
-                            })
-                            .create();
-                    chooseDialog.show();
+                        try {
+                            ClipboardManager clipboardManager = (ClipboardManager) context.getSystemService(Context.CLIPBOARD_SERVICE);
+                            if (clipboardManager != null) {
+                                clipboardManager.setPrimaryClip(ClipData.newPlainText(null, address));
+                            }
+                            Toast.makeText(context, context.getResources().getString(R.string.donate_bitconins_tips), Toast.LENGTH_SHORT).show();
+                        } catch (Throwable ignored) {
+                            ignored.printStackTrace();
+                        }
+                    }
                 })
-                .setNegativeButton(R.string.donate_dialog_no, ((dialog, which) -> {
-                    Intent intent = new Intent();
-                    intent.setAction(Intent.ACTION_VIEW);
-                    intent.addCategory(Intent.CATEGORY_BROWSABLE);
-                    intent.setData(Uri.parse("https://github.com/android-hacker/VirtualXposed"));
-                    context.startActivity(intent);
-                }))
                 .create();
-        try {
-            alertDialog.show();
-        } catch (Throwable ignored) {
-            // BadTokenException.
-        }
+        chooseDialog.show();
     }
 }
