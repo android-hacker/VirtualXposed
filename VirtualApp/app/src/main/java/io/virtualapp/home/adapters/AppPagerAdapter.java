fda8d0a66268e082d4393f30a1ca6c464c2b61e1
tiann <twsxtd@gmail.com>
2020-07-27 20:14:58 +0800

    Remove storage scan fragment
    Signed-off-by: tiann <twsxtd@gmail.com>

--- VirtualApp/app/src/main/java/io/virtualapp/home/adapters/AppPagerAdapter.java
+++ VirtualApp/app/src/main/java/io/virtualapp/home/adapters/AppPagerAdapter.java
@@ -1,23 +1,15 @@
 package io.virtualapp.home.adapters;
 
-import android.content.Context;
-import android.os.Build;
-import android.os.Environment;
-import android.os.storage.StorageManager;
-import android.os.storage.StorageVolume;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-
-import com.lody.virtual.helper.utils.DeviceUtil;
-import com.lody.virtual.helper.utils.Reflect;
 
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
-import io.virtualapp.XApp;
 import io.virtualapp.R;
+import io.virtualapp.XApp;
 import io.virtualapp.home.ListAppFragment;
 
 /**
@@ -31,29 +23,6 @@ public class AppPagerAdapter extends FragmentPagerAdapter {
         super(fm);
         titles.add(XApp.getApp().getResources().getString(R.string.clone_apps));
         dirs.add(null);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            Context ctx = XApp.getApp();
-            StorageManager storage = (StorageManager) ctx.getSystemService(Context.STORAGE_SERVICE);
-            for (StorageVolume volume : storage.getStorageVolumes()) {
-                //Why the fuck are getPathFile and getUserLabel hidden?!
-                //StorageVolume is kinda useless without those...
-                File dir = Reflect.on(volume).call("getPathFile").get();
-                String label = Reflect.on(volume).call("getUserLabel").get();
-                if (dir.listFiles() != null) {
-                    titles.add(label);
-                    dirs.add(dir);
-                }
-            }
-        } else {
-            // Fallback: only support the default storage sources
-            if (!DeviceUtil.isMeizuBelowN()) {
-                File storageFir = Environment.getExternalStorageDirectory();
-                if (storageFir != null && storageFir.isDirectory()) {
-                    titles.add(XApp.getApp().getResources().getString(R.string.external_storage));
-                    dirs.add(storageFir);
-                }
-            }
-        }
     }
 
     @Override
