104ce950f057649dd466e8df63bf3d92b3e5f86e
tiann <twsxtd@gmail.com>
2020-07-27 20:20:11 +0800

    Remove unused permission request.
    Signed-off-by: tiann <twsxtd@gmail.com>

--- VirtualApp/app/src/main/java/io/virtualapp/home/ListAppActivity.java
+++ VirtualApp/app/src/main/java/io/virtualapp/home/ListAppActivity.java
@@ -1,16 +1,11 @@
 package io.virtualapp.home;
 
-import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
@@ -37,26 +32,12 @@ public class ListAppActivity extends VActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clone_app);
-        mToolBar = (Toolbar) findViewById(R.id.clone_app_tool_bar);
-        mTabLayout = (TabLayout) mToolBar.findViewById(R.id.clone_app_tab_layout);
-        mViewPager = (ViewPager) findViewById(R.id.clone_app_view_pager);
+        mToolBar = findViewById(R.id.clone_app_tool_bar);
+        mTabLayout = mToolBar.findViewById(R.id.clone_app_tab_layout);
+        mViewPager = findViewById(R.id.clone_app_view_pager);
         mViewPager.setAdapter(new AppPagerAdapter(getSupportFragmentManager()));
         mTabLayout.setupWithViewPager(mViewPager);
 
-        // Request permission to access external storage
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            AlertDialog alertDialog = new AlertDialog.Builder(this, R.style.Theme_AppCompat_DayNight_Dialog_Alert)
-                    .setMessage(R.string.list_app_access_external_storage)
-                    .setPositiveButton(android.R.string.ok, (dialog, which) -> {
-                        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 0);
-                    })
-                    .create();
-            try {
-                alertDialog.show();
-            } catch (Throwable ignored) {
-                // BadTokenException.
-            }
-        }
     }
 
     @Override
@@ -66,15 +47,5 @@ public class ListAppActivity extends VActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        for (int result : grantResults) {
-            if (result == PackageManager.PERMISSION_GRANTED) {
-                mViewPager.setAdapter(new AppPagerAdapter(getSupportFragmentManager()));
-                break;
-            }
-        }
     }
 }
